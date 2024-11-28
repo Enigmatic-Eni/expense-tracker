9a3d9cbbd2574ed833794d0bfaa7b34565f4fe0a
hakeem <belloharyourmidey@gmail.com>
2024-11-28 18:28:50 +0100

    updating

---
+++ src/components/Sign_In.jsx
@@ -0,0 +1,15 @@
+function SignUp(){
+    return(
+        <div className="wrapper">
+            <div className="hero">
+                <img src="" alt="Hero image"/>
+            </div>
+            <div className="sign_in_page">
+                <form>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+export default SignUp;
