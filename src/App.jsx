b5c93bb3bc3f6b4c5f9a6325f606bfb78bb7987f
hakeem <belloharyourmidey@gmail.com>
2024-11-29 20:36:58 +0100

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import SignUp from './components/Sign_In'
 import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       {/* <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
         </Routes>
       </Router> */}
       <SignUp />
